23b3ee9662de7a3feb0e89faba86f3988b8b0422
Tiernan Martin <tiernan.martin@gmail.com>
2017-12-19 15:30:21 -0800

    revise script for creating the kc consolidated zoning

--- 2-communication/1-rnotebooks/tmp/tmp-make-kc-zoning.R
+++ 2-communication/1-rnotebooks/tmp/tmp-make-kc-zoning.R
@@ -16,7 +16,7 @@ library(miscgis)         # devtools::install_github("tiernanmartin/miscgis")
 library(snakecase)
 library(placement)       # devtools::install_github("DerekYves/placement")
 library(rprojroot)
-library(esri2sf)
+library(zip) 
 
 root <- rprojroot::is_rstudio_project
 root_file <- root$make_fix_file()
@@ -26,19 +26,45 @@ htmltools::tagList(rmarkdown::html_dependency_font_awesome())
 
 # King County Consolidated Zoning
 
-kc_zng_url <- "https://opendata.arcgis.com/datasets/7edc8f5c0efd4ef38f9bd7ebbdb51822_2493.geojson"
+# source: ftp://ftp.kingcounty.gov/gis-web/GISData/zoning_kc_consol_20_SHP.zip
 
-kc_zng <-  read_sf(kc_zng_url, stringsAsFactors = FALSE)
+kc_zng_fp <- root_file("1-data/2-external/zoning_kc_consol_20")
+
+kc_zng_load <- kc_zng_fp %>% 
+  make_or_read({
+    
+    dr_id <- as_id("12TZn4z6XzZ2fmvHyvDIXFHFQfmJ70hJM")
+    
+    zip_dir <- root_file("1-data/2-external")
+    
+    target_name <- "zoning_kc_consol_20"
+    
+    drive_read_zip(dr_id = dr_id,
+                   .tempdir = FALSE,
+                   dir_path = zip_dir,
+                   read_fun = read_sf,
+                   target_name = target_name,
+                   layer = "zoning_kc_consol_20", 
+                   stringsAsFactors = FALSE)
+    
+  },
+               {read_sf(kc_zng_fp, layer = "zoning_kc_consol_20", stringsAsFactors = FALSE)})
+
+kc_zng <- rename_if(kc_zng_load, not_sfc, to_screaming_snake_case)
 
 
 # SAVE & UPLOAD TO DRIVE ---- 
 
-kc_zng_fp <- root_file("1-data/2-external/code-enforcement-cases-2017.gpkg")
+kc_zng_gpkg_fp <- root_file("1-data/2-external/kc-zoning.gpkg")
+
+kc_zng_zip_fp <- root_file("1-data/2-external/kc-zoning.zip")
 
 drive_folder_id <- as_id("0B5Pp4V6eCkhrQ29lVGsxaS1ERXM") # ~/2-external/
 
-st_write(obj = kc_zng,dsn = kc_zng_fp, layer = 'kc_zoning_consolidated', driver = 'GPKG', layer_options = 'OVERWRITE=TRUE')
+st_write(obj = kc_zng,dsn = kc_zng_gpkg_fp, layer = 'kc_zoning_consolidated', driver = 'GPKG', layer_options = 'OVERWRITE=TRUE')
 
-drive_upload(media = kc_zng_fp, path = drive_folder_id)
+zip(kc_zng_zip_fp,kc_zng_gpkg_fp)
 
-drive_update(file = as_id(""), kc_zng_fp)
+# drive_upload(media = kc_zng_zip_fp, path = drive_folder_id)
+
+drive_update(file = as_id("1EQIDABm4gYdksu45ftYD7X4lueHZfvRt"), kc_zng_gpkg_fp)
